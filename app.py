8d262b53b50c5553615180dc3cd120e0249246a4
manikandanmohan10 <manikandan.mohan@softsuave.com>
2022-12-28 18:58:29 +0530

    Revert "test"
    This reverts commit ae3213199da481dac33b88c95f3437fb96b3c109.

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ app = Flask(__name__)
 def home():
     return "Hello World"
 
-@app.route('/revert')
-def revert():
-    return "Hello World"
-
 if __name__ == "__main__":
     app.run(debug=True)
+    
